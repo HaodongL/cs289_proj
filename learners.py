59db04de0336288964d2914cea3b0ae08e16624c
Haodong Li <haodong_li@berkeley.edu>
2022-04-27 10:58:21 -0700

    Lrnr_sl init

--- learners.py
+++ learners.py
@@ -3,6 +3,7 @@
 import numpy as np
 from abc import ABC, abstractmethod
 import statsmodels.api as sm
+from sklearn.model_selection import KFold
 
 
 class Learner(ABC):
@@ -22,11 +23,74 @@ class Learner(ABC):
         pass
 
 
+def initialize_sl(stack, name = None, params = None):
+
+    return Lrnr_sl(
+        stack = stack,
+        name = name,
+        params = params
+    )
+
+
+
+class Lrnr_sl(Learner):
+    def __init__(self, stack: list, name = None, params = None):
+        super().__init__(name = name, params = params)
+        self.stack = stack
+        self.folds = list(self.sl_task.cv_folds.split(self.sl_task.data["Y"]))
+        self.preds = None
+        self.fit_object_list = []
+
+
+    def train(self) -> None:
+        """Fit each learner on each training set,
+           save the predictions on each v set
+        
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        for k in range(len(self.folds)):
+            idx_t = self.folds[k][0]
+            idx_v = self.folds[k][1]
+            X_t = self.sl_task.data["X"][idx_t]
+            Y_t = self.sl_task.data["Y"][idx_t]
+            X_v = self.sl_task.data["X"][idx_v]
+            Y_v = self.sl_task.data["Y"][idx_v]
+
+            for l in range(len(self.stack)):
+                lrnr = self.stack[l]
+                fit_lrnr = lrnr.train(Y_t, X_t, family = lrnr.family).fit()
+                preds = fit_lrnr(X_v)
+
+
+
+
+
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Backward pass for f(z) = z.
+        
+        Parameters
+        ----------
+        X (new) input features for prediction
+
+        Returns
+        -------
+        predictons
+        """
+        fit = self.fit_object
+        preds = fit.predict(X)
+        return preds
 
 
 class Lrnr_glm(Learner):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name = None, params = None):
+        super().__init__(name = name, params = params)
         family = self.sl_task.family
         if (family == "Gaussian"):
             self.family = sm.families.Gaussian()
@@ -63,4 +127,3 @@ class Lrnr_glm(Learner):
         fit = self.fit_object
         preds = fit.predict(X)
         return preds
-
--- sl_task.py
+++ sl_task.py
@@ -1,9 +1,10 @@
 import numpy as np
 from abc import ABC, abstractmethod
+from sklearn.model_selection import KFold
 
 
 class sl_task(ABC):
-    def __init__(self, X, Y, family, cv_folds):
+    def __init__(self, X, Y, family, cv_folds: sklearn.model_selection._split.KFold):
         self.data = {"X" = X, "Y" = Y}
         self.family = family
         self.cv_folds = cv_folds
