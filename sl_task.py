a3479d10f19461072d8b975377a6576e56e4fadb
xiangli-jia <103897929+xiangli-jia@users.noreply.github.com>
2022-04-29 12:23:10 -0700

    dictionary typo

--- sl_task.py
+++ sl_task.py
@@ -5,6 +5,6 @@ from sklearn.model_selection import KFold
 
 class sl_task(ABC):
     def __init__(self, X, Y, family, cv_folds: sklearn.model_selection._split.KFold):
-        self.data = {"X" = X, "Y" = Y}
+        self.data = {"X": X, "Y": Y}
         self.family = family
         self.cv_folds = cv_folds
